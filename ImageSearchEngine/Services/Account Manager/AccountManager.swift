b02d757fd8eb1d2561d21106d10758ea6d9fafba
NikitaKelevra <nonamerz5@mail.ru>
2023-05-14 19:58:58 +0300

    Начало создания аккаунта на новой ветке

---
+++ ImageSearchEngine/Services/Account Manager/AccountManager.swift
@@ -0,0 +1,17 @@
+//
+//  AccountManager.swift
+//  ImageSearchEngine
+//
+//  Created by Сперанский Никита on 14.05.2023.
+//
+
+import Foundation
+
+protocol AccountChangable {
+    
+}
+
+final class AccountManager {
+    
+    
+}
